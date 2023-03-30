e37b16b10d8f8a1a8f3b485a78c61cd2ede83704
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-03-30 01:44:04 +0100

    Task 101

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,68 +1,40 @@
 #include "binary_trees.h"
-
+#include "9-binary_tree_height.c"
 /**
-* scale - aux function that measures the height of a binary tree
-* @tree: pointer to the root node of the tree to measure the height
-* Return: height of a binary tree, 0 is tree is NULL
-**/
-int scale(const binary_tree_t *tree)
+ *print_level - print the nodes by level
+ *@func: pointer to a functionn
+ *@level: the level
+ *@node: pointer to the node
+ *Return: nothing
+ */
+void print_level(binary_tree_t *node, void (*func)(int), int level)
 {
-	int ll = 1;
-	int lr = 1;
-
-	if (tree == NULL)
-	{
-		return (-1);
-	}
-	ll += scale((*tree).left);
-	lr += scale((*tree).right);
-	if (lr > ll)
-	{
-		return (lr);
-	}
-	return (ll);
-}
-
-/**
-* print_level - aux function to level travel
-* using level-order traversal
-* @tree: pointer to the root node of the tree to traverse
-* @func: pointer to a function to call for each node
-* @level: actual level
-**/
-void print_level(binary_tree_t *tree, int level, void (*func)(int))
+if (node != NULL && func != NULL)
 {
-	if (tree != NULL)
-	{
-		if (level == 0)
-		{
-			func((*tree).n);
-		}
-		else
-		{
-			print_level((*tree).left, level - 1, func);
-			print_level((*tree).right, level - 1, func);
-		}
-	}
+if (level == 1)
+func(node->n);
+if (level > 1)
+{
+print_level(node->left, func, level - 1);
+print_level(node->right, func, level - 1);
 }
-
+}
+}
 /**
-* binary_tree_levelorder - goes through a binary tree
-* using level-order traversal
-* @tree: pointer to the root node of the tree to traverse
-* @func: pointer to a function to call for each node
-**/
+ *binary_tree_levelorder - level-order traversal
+ *@tree: pointer to the tree
+ *@func: pointer to a function
+ *Return: nothing
+ */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	int level, height;
+size_t height = 0;
+size_t counter;
+binary_tree_t *copy_tree = (binary_tree_t *)tree;
 
-	if (tree != NULL && func != NULL)
-	{
-		height = scale(tree) + 1;
-/*		printf("Level: %i\n", height);*/
-		for (level = 0; level < height; level++)
-		{
-			print_level((binary_tree_t *)tree, level, func);
-		}
-	}
+if (tree == NULL || func == NULL)
+return;
+height = binary_tree_height(tree);
+for (counter = 0; counter <= height + 1; counter++)
+print_level(copy_tree, func, counter);
 }
